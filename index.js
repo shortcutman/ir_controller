d6a69dbd0acf572782335341bff4b49ebe250d25
Daniel Burke <danieljburke@me.com>
2020-08-13 16:31:50 +1000

    added commands and timer for auto-off

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ const child_process = require('child_process')
 const app = express()
 app.use(express.urlencoded())
 
+var timeout;
+
+function runIRCommand(command) {
+  const fullCommand = 'irsend send_once hk970.conf.conf ' + command
+  child_process.exec(fullCommand, (error, stdout, stderr) => {
+    console.log('Error', error)
+    console.log('stdout', stdout)
+    console.log('stderr', stderr)
+  })
+}
+
 app.post('/raspotify', (request, response) => {
   console.log('Received player event:', request.body.PLAYER_EVENT)
   response.send('Received')
@@ -12,19 +23,29 @@ app.post('/raspotify', (request, response) => {
   switch (request.body.PLAYER_EVENT) {
     case 'start':
       console.log('turn on stereo')
+      runIRCommand('poweron')
       console.log('invalidate timer')
-
-      child_process.exec('irsend send_once hk970.conf.conf poweron', (error, stdout, stderr) => {
-        console.log('Error', error)
-        console.log('stdout', stdout)
-        console.log('stderr', stderr)
-      })
+      clearTimeout(timeout)
       break;
 
     case 'stop':
       console.log('start stereo off timer')
+
+      timeout = setTimeout(() => {
+        runIRCommand('poweroff')
+      }, 60000)
       break;
   }
+})
+
+app.get('/volumeup', (request, response) => {
+  console.log('turn volume up')
+  runIRCommand('volumeup')
+})
+
+app.get('/volumedown', (request, response) => {
+  console.log('turn volume down')
+  runIRCommand('volumedown')
 })
 
 app.listen(8111, () => {
